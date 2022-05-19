2fa0b59c4f7a42dd30bda07445ef641208b41077
unknown <emmanuel.brim@yahoo.com>
2022-05-19 14:21:06 -0400

    updating logic files

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -2,37 +2,39 @@
 console.log("working");
 // Create the map object with center at the San Francisco airport.
 let map = L.map('mapid',{
-        center: [30, 30],
-        zoom: 2
+        center: [43.7, -79.3],
+        zoom: 11
 });
 //Add tilelayer1
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
+let street = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        accessToken: API_KEY
+    });
 //Add tilelayer2
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satellite = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
 let baseLayer = {
-    layer1 : light,
-    layer2 : dark
+    layer1 : street,
+    layer2 : satellite
 };
  let myStyle = {
      color : "yellow",
      weight: 2
  }
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/emmanuelbrim/Mapping_Earthquakes/main/torontoRoutes.json";
+let airportData = "https://raw.githubusercontent.com/emmanuelbrim/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
+    console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data,{ style : myStyle,      
-      onEachFeature:function(feature, layer){layer.bindPopup(`<h3> Airport code: ${feature.properties.airline} <hr> <h4> Airport name: ${feature.properties.dst}`)}}).addTo(map);
+  L.geoJSON(data, {
+      onEachFeature:function(feature, layer){layer.bindPopup(`<h2> ${feature.properties.AREA_NAME} <h2>`)}
+  }).addTo(map);
 });
 //Add control layer
 let control = L.control.layers(baseLayer).addTo(map);
